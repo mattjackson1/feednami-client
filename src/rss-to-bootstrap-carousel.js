33b8fcf9811201683c8f6a1201c7746a74bebdc6
Matt Jackson <mattjackson1@gmail.com>
2019-09-16 09:04:53 +0100

    Added code to extend JQuery and to create Bootstrap elements from the feednami output

---
+++ src/rss-to-bootstrap-carousel.js
@@ -0,0 +1,99 @@
+// EXAMPLE USAGE
+// $(".carousel-inner").showRSS("https://www.burytimes.co.uk/news/rss/");
+
+
+$.fn.extend({
+  showRSS: function(url, num) {
+    if (num === undefined) {
+      num = 10;
+    }
+
+    return this.each(function() {
+
+      var currentObject = $(this); // each DOM element in this loop
+
+      feednami.load(url, function(result) {
+        if (result.error) {
+          console.log(result.error);
+        } else {          
+          var entries = result.feed.entries;
+          for (var i = 0; i < num; i++) {
+            
+            var entry = entries[i];
+            
+            var itemClass = 'item';
+            i === 0 ? itemClass+=' active' : null;
+            
+            var $div = $("<div>", {"class": itemClass}).append(
+              $("<a>", {href: entry.link, target: "_blank", text: entry.title})
+            )
+            
+            currentObject.append($div);
+          }
+        }
+      });
+    });
+  }
+});
+
+/*!
+ * Feednami Client v1.0.1 - DONT CHANGE BELOW THIS LINE
+ * Copyright (c) 2015 Richard Kazuo Miller and contributors
+ */
+
+window.feednami = {};
+feednami.load = function(options, callback) {
+  var apiRoot = "https://api.feednami.com/api/v1";
+  var feedUrl = options;
+  if (typeof options == "object") {
+    feedUrl = options.url;
+  }
+  var qs = "url=" + feedUrl;
+  if (options.format) {
+    qs += "&include_xml_document&format=" + options.format;
+  }
+  if (options.includeXml) {
+    qs += "&include_xml_document";
+  }
+  var url = apiRoot + "/feeds/load?" + qs;
+  if (window.XDomainRequest) {
+    var script = document.createElement("script");
+    var callbackName =
+        "jsonp_callback_" +
+        new Date().getTime() +
+        "_" +
+        Math.round(1000000 * Math.random());
+    url += "&jsonp_callback=" + callbackName;
+    window[callbackName] = function(data) {
+      callback(data);
+      document.body.removeChild(script);
+      window[callbackName] = null;
+      try {
+        delete window[callbackName];
+      } catch (e) {}
+    };
+    script.src = url;
+    //document.body.appendChild(script);
+  } else {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+      if (req.readyState == 4) {
+        callback(JSON.parse(req.responseText));
+      }
+    };
+    req.open("GET", url);
+    req.send();
+  }
+};
+/*
+feednami.loadGoogleFormat = function(feedUrl, callback) {
+  return feednami.load(
+    {
+      url: feedUrl,
+      format: "google",
+      includeXml: true
+    },
+    callback
+  );
+};
+*/
